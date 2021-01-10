30c27acecfe7286482be55c193b65889e0cf9635
Yousuf Borno <yb.26.code@gmail.com>
2021-01-09 20:50:41 -0500

    AppBar is allset

---
+++ imagefinder/src/components/navbar/NavBar.js
@@ -0,0 +1,27 @@
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+
+
+const NavBar = ()=>{
+    
+
+    return (
+
+        <div >
+
+
+      <AppBar color ='secondary' >
+
+      <Typography variant="h3">Photo Finder</Typography>
+
+      </AppBar>
+       
+      
+    </div>
+    )
+}
+
+
+export default NavBar;
